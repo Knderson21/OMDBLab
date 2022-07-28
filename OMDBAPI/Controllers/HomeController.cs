d41a6c79eaf781647ac62bb33ff7709280e9b2e3
Krista <knderson21@gmail.com>
2022-07-28 08:23:14 -0400

    OMDB Movie Database API

---
+++ OMDBAPI/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMDBAPI.Models;
+using System.Diagnostics;
+
+namespace OMDBAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public MovieDAL movieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]//Run when you first go to page
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]//Run when you submit your form
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel movie = movieDAL.GetMovie(title);
+            return View(movie);
+        }
+
+        [HttpGet]//Run when you first go to page
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+        [HttpPost]//Run when you submit your form
+        public IActionResult MovieNight(string title1, string title2, string title3)
+        {
+            List<MovieModel> result = new List<MovieModel>
+            {
+                movieDAL.GetMovie(title1), 
+                movieDAL.GetMovie(title2), 
+                movieDAL.GetMovie(title3)
+            };
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OMDBAPI/Models/MovieDAL.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace OMDBAPI.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string title)
+        {
+            string key = "350805ef"; //this API Key should be hidden
+            string url = $"http://www.omdbapi.com/?apikey={key}&t={title}";
+
+            //setup request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //capture response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //save response data
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+
+            //***
+            //convert string of JSON into a C# object <--ADJUST--
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+    }
+}
